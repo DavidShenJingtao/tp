3a9287dbb9f39b2874e06687461f270abc022149
BJoozz <123baoek31@gmail.com>
2025-10-16 23:05:01 +0800

    WIP: add comparator for sorting list and listsession commands

--- src/main/java/seedu/address/logic/commands/ListSessionCommand.java
+++ src/main/java/seedu/address/logic/commands/ListSessionCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
+import seedu.address.model.person.PersonTypeComparator;
 import seedu.address.model.person.SessionMatchPredicate;
 
 /**
@@ -29,6 +30,7 @@ public class ListSessionCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
+        //model.sortFilteredPersonList(new PersonTypeComparator());
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,14 +4,17 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonTypeComparator;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -126,6 +129,14 @@ public class ModelManager implements Model {
     public void updateFilteredPersonList(Predicate<Person> predicate) {
         requireNonNull(predicate);
         filteredPersons.setPredicate(predicate);
+        //sortFilteredPersonList(new PersonTypeComparator());
+    }
+
+    @Override
+    public void sortFilteredPersonList(Comparator<Person> comparator) {
+        requireNonNull(comparator);
+        FXCollections.sort(addressBook.getPersonList(), comparator);
+        //Collections.sort(list, comparator);
     }
 
     @Override
