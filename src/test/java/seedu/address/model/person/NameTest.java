478ca3500d890d1596849ecdd8dc34e46331bab3
Haxatron <you@example.com>
2025-10-24 02:43:35 +0800

    Enforce 500 name length limit

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -29,6 +29,7 @@ public class NameTest {
         assertFalse(Name.isValidName(" ")); // spaces only
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
         assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("A".repeat(Name.MAX_NAME_LENGTH + 1))); // more than MAX_NAME_LENGTH letters
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
@@ -36,6 +37,7 @@ public class NameTest {
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("A".repeat(Name.MAX_NAME_LENGTH))); // equal MAX_NAME_LENGTH letters
     }
 
     @Test
